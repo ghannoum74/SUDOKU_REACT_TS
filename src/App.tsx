5e3275a1054f0d791ee049dc657e929ce4057974
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-08 04:59:22 +0200

    create the main containers and render the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import GameNavbar from "./components/GameNavbar";
+import SudokuInputs from "./components/SudokuInputs";
+import SudokuSetting from "./components/SudokuSetting";
+import SudokuShape from "./components/SudokuShape";
 import "./css/App.css";
 
 function App() {
-  return <>hello</>;
+  return (
+    <div className="main-container">
+      <GameNavbar />
+      <div className="game-container">
+        <SudokuShape />
+        <div className="setting-container">
+          <SudokuSetting />
+          <SudokuInputs />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
