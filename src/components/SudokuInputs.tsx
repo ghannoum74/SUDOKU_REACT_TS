ee54251742b66dc41a24e5bceddad116dcd0e613
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-12 13:12:03 +0200

    create the new game menu

--- src/components/SudokuInputs.tsx
+++ src/components/SudokuInputs.tsx
@@ -1,4 +1,9 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRotateLeft } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
 const SudokuInputs = () => {
+  const [newGame, setNewGame] = useState(false);
   const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <div className="sudoku-inputs-container">
@@ -6,8 +11,51 @@ const SudokuInputs = () => {
         {numbers.map((val, key) => (
           <li key={key}>{val}</li>
         ))}
-        <li className="new-game">New Game</li>
+        <li
+          className="new-game"
+          style={{ userSelect: "none" }}
+          onClick={() => setNewGame((prev) => !prev)}
+        >
+          New Game
+        </li>
       </ul>
+      {newGame && (
+        <div className={`new-game-menu ${newGame ? "appear" : ""}`}>
+          <div className="head">Select Game Mode</div>
+          <div className="small">Current game progress will be lost</div>
+          <div className="mode-btn">Classic</div>
+          <ul className="level">
+            <div className="level-container">
+              <li>
+                <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
+              </li>
+              <li>Easy</li>
+            </div>
+            <div className="level-container">
+              <li>
+                <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
+              </li>
+              <li>Medium</li>
+            </div>
+            <div className="level-container">
+              <li>
+                <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
+              </li>
+              <li>Hard</li>
+            </div>
+            <div className="level-container">
+              <li>
+                <FontAwesomeIcon
+                  icon={faRotateLeft}
+                  style={{ color: "#325aaf" }}
+                  size="lg"
+                />
+              </li>
+              <li>restart</li>
+            </div>
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
