55f27b4c541f0252dbaaa3c36753c4274b0835b7
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-17 22:05:30 +0200

    create mistake counter state

---
+++ src/states/mistakesNumber.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+type mistakenumber = {
+  mistakesNb: number | null;
+};
+
+const initialState: mistakenumber = {
+  mistakesNb: 0,
+};
+
+export const setMistakes = createSlice({
+  name: "pickingNumber",
+  initialState,
+  reducers: {
+    setMistakeNumber: (state, action: PayloadAction<number>) => {
+      state.mistakesNb = action.payload;
+    },
+  },
+});
+
+// Correct the export here:
+export const { setMistakeNumber } = setMistakes.actions;
+export default setMistakes.reducer;
