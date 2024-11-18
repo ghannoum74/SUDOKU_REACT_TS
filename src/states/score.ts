247b4a2c99d897b37117e5fe92cc818e75818e20
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-18 02:26:04 +0200

    add the switch difficulties ti calculate the score

--- src/states/score.ts
+++ src/states/score.ts
@@ -1,19 +1,44 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 type mistakenumber = {
-  score: number | null;
+  score: number;
+  level: "easy" | "medium" | "hard" | "expert" | "";
 };
 
 const initialState: mistakenumber = {
   score: 0,
+  level: "",
 };
 
 export const sudokuScore = createSlice({
   name: "sudokuScore",
   initialState,
   reducers: {
-    setScore: (state, action: PayloadAction<number>) => {
-      state.score = action.payload;
+    setScore: (
+      state,
+      action: PayloadAction<"easy" | "medium" | "hard" | "expert">
+    ) => {
+      switch (action.payload) {
+        case "easy": {
+          state.score += 50;
+          break;
+        }
+        case "medium": {
+          state.score += 100;
+          break;
+        }
+        case "hard": {
+          state.score += 150;
+          break;
+        }
+        case "expert": {
+          state.score += 250;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
     },
   },
 });
