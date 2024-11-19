abb57e118ed729925431594bdf40f5328f80cb9c
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-19 13:45:52 +0200

    remove the gamer over state

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import SudokuInputs from "./components/SudokuInputs";
 import SudokuSetting from "./components/SudokuSetting";
 import SudokuShape from "./components/SudokuShape";
 import "./css/App.css";
-import { useState } from "react";
 import GameOver from "./components/GameOver";
+import { useSelector } from "react-redux";
+import { RootState } from "./states/store";
 
 function App() {
-  const [gameOver, setGameOver] = useState<boolean>(false);
-
+  // const [gameOver, setGameOver] = useState<boolean>(false);
+  const isGameOver = useSelector((state: RootState) => state.timer.isGameOver);
   return (
     <div className="main-container">
-      {gameOver && <GameOver setGameOver={setGameOver} />}
-      <GameNavbar gameOver={gameOver} />
+      {isGameOver && <GameOver />}
+      <GameNavbar />
 
       <div className="game-container">
-        <SudokuShape setGameOver={setGameOver} />
+        <SudokuShape />
         <div className="setting-container">
           <SudokuSetting />
           <SudokuInputs />
