bec515057aef3b515183b851e276068bc9e247b9
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-20 15:39:08 +0200

    impliment the functionality to render the congratulation ui

--- src/states/score.ts
+++ src/states/score.ts
@@ -3,11 +3,13 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 type mistakenumber = {
   score: number;
   level: "easy" | "medium" | "hard" | "expert" | "";
+  isSolved: boolean;
 };
 
 const initialState: mistakenumber = {
   score: 0,
   level: "easy",
+  isSolved: false,
 };
 
 export const sudokuScore = createSlice({
@@ -41,12 +43,41 @@ export const sudokuScore = createSlice({
         }
       }
     },
+
     resetScore: (state) => {
       state.score = 0;
+    },
+    checkScore: (
+      state,
+      action: PayloadAction<"easy" | "medium" | "hard" | "expert" | "">
+    ) => {
+      state.isSolved = false;
+      switch (action.payload) {
+        case "easy": {
+          state.isSolved = state.score === 1500;
+          break;
+        }
+        case "medium": {
+          state.isSolved = state.score === 5000;
+          break;
+        }
+        case "hard": {
+          state.isSolved = state.score === 9000;
+          break;
+        }
+        case "expert": {
+          state.isSolved = state.score === 18750;
+          break;
+        }
+        default: {
+          state.isSolved = false;
+          break;
+        }
+      }
     },
   },
 });
 
 // Correct the export here:
-export const { setScore, resetScore } = sudokuScore.actions;
+export const { setScore, resetScore, checkScore } = sudokuScore.actions;
 export default sudokuScore.reducer;
