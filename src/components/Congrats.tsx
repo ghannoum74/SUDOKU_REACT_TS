dd622d8bc375ad0adadbf8dcd36f4fe4ede1da6c
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-20 15:37:37 +0200

    import the data nescessery for the ui

--- src/components/Congrats.tsx
+++ src/components/Congrats.tsx
@@ -1,26 +1,62 @@
+import { useSelector } from "react-redux";
+import { RootState } from "../states/store";
+
 const Congrats = () => {
+  const score = useSelector((state: RootState) => state.score.score);
+  const level = useSelector((state: RootState) => state.score.level);
+  const seconds = useSelector((state: RootState) => state.timer.seconds);
+  const minutes = useSelector((state: RootState) => state.timer.minutes);
+  const hours = useSelector((state: RootState) => state.timer.hours);
+  const fullSudokuShape = useSelector(
+    (state: RootState) => state.setSolvedData.solvedBoardData
+  );
+
+  console.log(fullSudokuShape);
   return (
     <div className="congrat-container">
       <div className="left-side">
+        <img src="https://sudoku.com/img/rays.png" />
         <h1>Excellent!</h1>
-        <div className="solved-sudoku">solved sudoku</div>
+        <table className="table-shape">
+          <tbody>
+            {fullSudokuShape.map((row, key) => (
+              <tr key={key} id={`${key}`}>
+                {row.map((cell, key) => (
+                  <td key={key} id={cell.id}>
+                    <div id={cell.id}>{cell.value}</div>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
       <div className="right-side">
         <div className="position">
           <div className="">Tournament Positions</div>
           <button>See All</button>
         </div>
+        <div className="name-id">
+          <div>Name</div>
+          <div>Name</div>
+        </div>
         <div className="score">
-          <div className="">Score</div>
-          <div>Score</div>
-        </div>
-        <div className="difficulty">
-          <div className="">Difficulty</div>
-          <div>easy</div>
-        </div>
-        <div className="time">
-          <div className="">Time</div>
-          <div>time</div>
+          <div>
+            <div className="">Score</div>
+            <div>{score}</div>
+          </div>
+          <div className="difficulty">
+            <div className="">Difficulty</div>
+            <div>{level}</div>
+          </div>
+          <div className="time">
+            <div className="">Time</div>
+            <div>
+              {hours <= 9 ? `0${hours}` : hours}:
+              {minutes <= 9 ? `0${minutes}` : minutes}:
+              {seconds <= 9 ? `0${seconds}` : seconds}
+            </div>
+          </div>
         </div>
         <button className="new-game">New Game</button>
       </div>
