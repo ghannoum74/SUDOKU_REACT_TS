ea2764005f2d82e9ec10ac1b8846bd76663c6453
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-22 00:21:15 +0200

    create the APIS for the get and post data

--- server/server.ts
+++ server/server.ts
@@ -1,48 +1,63 @@
 import express, { Request, Response } from "express";
 import { User } from "./models/user";
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import cors from "cors";
 import multer from "multer";
 
 dotenv.config();
 
 const app = express();
-const upload = multer({ dest: "uploads/" });
 app.use(express.json());
 app.use(cors());
 
-app.set(
-  "/",
-  upload.single("profileImage"),
+const defaultImagePath =
+  "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg";
+
+app.post(
+  "/addScore",
+  multer().single("image"),
   async (req: Request, res: Response) => {
-    const imageUrl = req.file.path;
-    const { username, score } = req.body;
+    const { username, score, level, time } = req.body;
 
     try {
-      const existingName = await User.findOne(username);
+      const imageUrl = req.file?.path || defaultImagePath;
+      const existingName = await User.findOne({ username });
       if (existingName) {
-        return res.status(400).json({ message: "username already taken" });
-      }
+        res.status(400).json({ message: "username already taken" });
+      } else {
+        const user = await User.create({
+          username,
+          score,
+          profileImg: imageUrl,
+          level: level,
+          time: time,
+        });
+        console.log(user);
 
-      const user = await User.create({
-        username,
-        score,
-        profileImage: imageUrl,
-      });
-      res.status(200).json({ user });
+        res.status(200).json({ user });
+      }
     } catch (error) {
-      console.log(error);
+      res.status(400).json(error);
     }
   }
 );
 
+app.get("/getUsers", async (req: Request, res: Response) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
 mongoose
   .connect(process.env.DATABASE_URL as string)
   .then(() => {
-    app.listen(process.env.PORT as string, () => {
+    app.listen(3000, () => {
       console.log(
-        `Server is running on port ${process.env.PORT}\nConnected to dataBase...`
+        `Server is running on port ${3000}\nConnected to dataBase...`
       );
     });
   })
