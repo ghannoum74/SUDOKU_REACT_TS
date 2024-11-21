342a1c58f9f0c957dbcd352224ea19125beed891
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-21 20:37:22 +0200

    create difficulty menu as component

---
+++ src/components/DifficultuMenu.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { chooseDifficulty } from "../states/difficultyGame";
+import { useDispatch } from "react-redux";
+
+interface diffState {
+  newGame: boolean;
+}
+
+const DifficultuMenu: React.FC<diffState> = ({ newGame }) => {
+  type Level = "easy" | "medium" | "hard" | "expert";
+  const dispatch = useDispatch();
+
+  const handleLevel = (e: React.MouseEvent<HTMLElement>) => {
+    dispatch(chooseDifficulty(e.currentTarget.id as Level));
+  };
+  return (
+    <div className={`new-game-menu ${newGame ? "appear" : ""}`}>
+      <div className="head">Select Game Mode</div>
+      <div className="small">Current game progress will be lost</div>
+      <div className="mode-btn">Classic</div>
+      <ul className="level">
+        <div className="level-container" id="easy" onClick={handleLevel}>
+          <li>
+            <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
+          </li>
+          <li>Easy</li>
+        </div>
+        <div className="level-container" id="medium" onClick={handleLevel}>
+          <li>
+            <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
+          </li>
+          <li>Medium</li>
+        </div>
+        <div className="level-container" id="hard" onClick={handleLevel}>
+          <li>
+            <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
+          </li>
+          <li>Hard</li>
+        </div>
+        <div className="level-container" id="expert" onClick={handleLevel}>
+          <li>
+            <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
+          </li>
+          <li>Expert</li>
+        </div>
+      </ul>
+    </div>
+  );
+};
+
+export default DifficultuMenu;
