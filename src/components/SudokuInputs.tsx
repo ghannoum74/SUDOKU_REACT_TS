c19a932cfd01718463e0f0321707694165d21276
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-21 20:38:37 +0200

    remove the pattern number to add the leader board

--- src/components/SudokuInputs.tsx
+++ src/components/SudokuInputs.tsx
@@ -1,77 +1,23 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect, useState } from "react";
 
-import { chooseDifficulty } from "../states/difficultyGame";
+import DifficultuMenu from "./DifficultuMenu";
 
 const SudokuInputs = () => {
-  type Level = "easy" | "medium" | "hard" | "expert";
   const [newGame, setNewGame] = useState(false);
-  const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  const dispatch = useDispatch();
-
-  const handleLevel = (e: React.MouseEvent<HTMLElement>) => {
-    dispatch(chooseDifficulty(e.currentTarget.id as Level));
-  };
-
+  useEffect(() => {}, []);
   return (
     <div className="sudoku-inputs-container">
-      <ul className="number-container">
-        {numbers.map((val, key) => (
-          <li key={key} data-val={val}>
-            {val}
-          </li>
-        ))}
-        <li
+      <div className="number-container">
+        <div className="leader-board">{localStorage.getItem("username")}</div>
+        <div
           className="new-game"
           style={{ userSelect: "none" }}
           onClick={() => setNewGame((prev) => !prev)}
         >
           New Game
-        </li>
-      </ul>
-      {newGame && (
-        <div className={`new-game-menu ${newGame ? "appear" : ""}`}>
-          <div className="head">Select Game Mode</div>
-          <div className="small">Current game progress will be lost</div>
-          <div className="mode-btn">Classic</div>
-          <ul className="level">
-            <div className="level-container" id="easy" onClick={handleLevel}>
-              <li>
-                <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
-              </li>
-              <li>Easy</li>
-            </div>
-            <div className="level-container" id="medium" onClick={handleLevel}>
-              <li>
-                <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
-              </li>
-              <li>Medium</li>
-            </div>
-            <div className="level-container" id="hard" onClick={handleLevel}>
-              <li>
-                <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
-              </li>
-              <li>Hard</li>
-            </div>
-            <div className="level-container" id="expert" onClick={handleLevel}>
-              <li>
-                <img src="https://cdn-icons-png.flaticon.com/512/4295/4295600.png" />
-              </li>
-              <li>Expert</li>
-            </div>
-            {/* <div className="level-container" id="restart" onClick={handleLevel}>
-              <li>
-                <FontAwesomeIcon
-                  icon={faRotateLeft}
-                  style={{ color: "#325aaf" }}
-                  size="lg"
-                />
-              </li>
-              <li>restart</li>
-            </div> */}
-          </ul>
         </div>
-      )}
+      </div>
+      {newGame && <DifficultuMenu newGame={newGame} />}
     </div>
   );
 };
