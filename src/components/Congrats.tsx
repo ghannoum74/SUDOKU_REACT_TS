535b2d632f3745898b0261cc096f4a19c85449e4
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-21 22:04:04 +0200

    add the file reader for the walpaper

--- src/components/Congrats.tsx
+++ src/components/Congrats.tsx
@@ -1,7 +1,11 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../states/store";
+import React, { useEffect, useState } from "react";
+import { pause } from "../states/timer";
 
 const Congrats = () => {
+  const defaultProfile =
+    "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-88wkdmjrorckekha.jpg";
   const score = useSelector((state: RootState) => state.score.score);
   const level = useSelector((state: RootState) => state.score.level);
   const seconds = useSelector((state: RootState) => state.timer.seconds);
@@ -10,35 +14,64 @@ const Congrats = () => {
   const fullSudokuShape = useSelector(
     (state: RootState) => state.setSolvedData.solvedBoardData
   );
+  const dispatch = useDispatch();
+  const [name, setName] = useState<string>("");
+  const [imageSrc, setImageSrc] = useState<string | null>(null);
 
-  console.log(fullSudokuShape);
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleSaveName = () => {
+    localStorage.setItem("username", name);
+    location.reload();
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setImageSrc(reader.result as string);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
+  useEffect(() => {
+    // pause the timer
+    dispatch(pause());
+  }, []);
+
   return (
     <div className="congrat-container">
       <div className="left-side">
         <img src="https://sudoku.com/img/rays.png" />
         <h1>Excellent!</h1>
-        <table className="table-shape">
-          <tbody>
+        <div className="outline-table">
+          <div className="table-shape">
             {fullSudokuShape.map((row, key) => (
-              <tr key={key} id={`${key}`}>
+              <div key={key} id={`${key}`} className="tr">
                 {row.map((cell, key) => (
-                  <td key={key} id={cell.id}>
+                  <div key={key} id={cell.id} className="td">
                     <div id={cell.id}>{cell.value}</div>
-                  </td>
+                  </div>
                 ))}
-              </tr>
+              </div>
             ))}
-          </tbody>
-        </table>
+          </div>
+        </div>
       </div>
       <div className="right-side">
-        <div className="position">
-          <div className="">Tournament Positions</div>
-          <button>See All</button>
+        <div className="profile-picture">
+          <img src={imageSrc ? imageSrc : defaultProfile} alt="Profile" />
+          <input type="file" accept="image/*" onChange={handleFileChange} />
         </div>
         <div className="name-id">
           <div>Name</div>
-          <div>Name</div>
+          <div className="name-place">
+            <input type="text" autoFocus onChange={handleInput} />
+          </div>
         </div>
         <div className="score">
           <div>
@@ -58,7 +91,24 @@ const Congrats = () => {
             </div>
           </div>
         </div>
-        <button className="new-game">New Game</button>
+        <div className="btns">
+          <button
+            className="new-game save"
+            onClick={handleSaveName}
+            style={{
+              cursor: `${
+                RegExp("^.{3,}$").test(name.trim()) ? "pointer" : "not-allowed"
+              }`,
+            }}
+            disabled={RegExp("^.{3,}$").test(name.trim()) ? false : true}
+          >
+            Save Your Record
+          </button>
+          <button className="new-game" onClick={() => location.reload()}>
+            New Game
+          </button>
+        </div>
+        {/* {newGame && <DifficultuMenu newGame={newGame} />} */}
       </div>
     </div>
   );
