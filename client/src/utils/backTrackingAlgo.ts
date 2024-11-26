e0ea5c00796555423a6f37196e2d5877c4daedb2
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-26 19:42:29 +0200

    use the cell type

--- client/src/utils/backTrackingAlgo.ts
+++ client/src/utils/backTrackingAlgo.ts
@@ -1,14 +1,4 @@
-interface Cell {
-  value: number | null;
-  calculate: boolean;
-  id: string;
-  row: number;
-  column: number;
-  block: number;
-  matrix: string;
-  unchangebale: boolean;
-  hinted: boolean;
-}
+import { Cell } from "../types/cell";
 
 type PuzzleResult = {
   emptyBoard: Cell[][];
