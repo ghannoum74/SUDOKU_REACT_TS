4a84ee9b0c1466f62ee0d7d7339623778cc919f5
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-26 19:43:21 +0200

    use type cell

--- client/src/utils/hint.ts
+++ client/src/utils/hint.ts
@@ -1,16 +1,5 @@
+import { Cell } from "../types/cell";
 import { getShuffledNumbers, isValidPlacement } from "./backTrackingAlgo";
-
-interface Cell {
-  value: number | null;
-  calculate: boolean;
-  id: string;
-  row: number;
-  column: number;
-  block: number;
-  matrix: string;
-  unchangebale: boolean;
-  hinted: boolean;
-}
 
 export const giveHint = (board: Cell[][]): Cell[][] => {
   const tempBoard = JSON.parse(JSON.stringify(board));
@@ -36,42 +25,3 @@ export const giveHint = (board: Cell[][]): Cell[][] => {
 
   return tempBoard;
 };
-
-// import { getShuffledNumbers, isValidPlacement } from "./backTrackingAlgo";
-
-// interface Cell {
-//   value: number | null;
-//   calculate: boolean;
-//   id: string;
-//   row: number;
-//   column: number;
-//   block: number;
-//   matrix: string;
-//   unchangebale: boolean;
-// }
-
-// export const giveHint = (
-//   board: Cell[][]
-// ): { tempBoard: Cell[][]; row: number | null; column: number | null } => {
-//   const tempBoard = JSON.parse(JSON.stringify(board));
-
-//   // loop using shuffled array number for the hint be not in order
-//   const shuffleRow = getShuffledNumbers();
-//   const shuffleColumn = getShuffledNumbers();
-//   for (const row of shuffleRow) {
-//     for (const col of shuffleColumn) {
-//       if (tempBoard[row - 1][col - 1].value === null) {
-//         for (let i = 1; i <= 9; i++) {
-//           if (isValidPlacement(tempBoard, row - 1, col - 1, i)) {
-//             tempBoard[row - 1][col - 1].value = i;
-//             // set the hinted cell unchangebale to avoid erase it
-//             tempBoard[row - 1][col - 1].unchangebale = true;
-//             return { tempBoard: tempBoard, row: row - 1, column: col - 1 };
-//           }
-//         }
-//       }
-//     }
-//   }
-
-//   return { tempBoard: board, row: null, column: null };
-// };
