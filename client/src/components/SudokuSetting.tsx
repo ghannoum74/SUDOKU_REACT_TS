2e927200f620210529d8716bbe6e98433ff9660a
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-27 04:23:03 +0200

    replace the hint by leader box

--- client/src/components/SudokuSetting.tsx
+++ client/src/components/SudokuSetting.tsx
@@ -1,60 +1,16 @@
-import {
-  faArrowRotateLeft,
-  faEraser,
-  faLightbulb,
-  faPencil,
-} from "@fortawesome/free-solid-svg-icons";
+import { faCrown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useDispatch, useSelector } from "react-redux";
-import { setHint } from "../states/hint";
-import { RootState } from "../states/store";
 
 const SudokuSetting = () => {
-  const hint = useSelector((state: RootState) => state.hint.hint);
-  const dispatch = useDispatch();
   return (
     <div className="sudoku-setting-container">
       <ul className="sudoku-setting-icons">
-        <div className="icon-container">
-          <li>
-            <FontAwesomeIcon
-              icon={faArrowRotateLeft}
-              size="2xl"
-              style={{ color: "#325aaf" }}
-            />
-          </li>
-          <div className="caption">Undo</div>
-        </div>
-        <div className="icon-container">
-          <li>
-            <FontAwesomeIcon
-              icon={faEraser}
-              size="2xl"
-              style={{ color: "#325aaf" }}
-            />
-          </li>
-          <div className="caption">Erase</div>
-        </div>
-        <div className="icon-container">
-          <li>
-            <FontAwesomeIcon
-              icon={faPencil}
-              size="2xl"
-              style={{ color: "#325aaf" }}
-            />
-          </li>
-          <div className="caption">Notes</div>
-        </div>
-        <div className="icon-container">
-          <li className="hint" onClick={() => dispatch(setHint())}>
-            <FontAwesomeIcon
-              icon={faLightbulb}
-              size="2xl"
-              style={{ color: "#325aaf" }}
-            />
-            <div className="hint-number">{hint}</div>
-          </li>
-          <div className="caption">Hint</div>
+        <div className="header-box">
+          <h3 className="header">Leader Board</h3>
+          <FontAwesomeIcon
+            icon={faCrown}
+            style={{ color: "#ffffff", fontSize: "2rem" }}
+          />
         </div>
       </ul>
     </div>
