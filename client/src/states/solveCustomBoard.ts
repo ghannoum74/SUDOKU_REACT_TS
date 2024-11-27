f2bef4156e61f7ef03867db4328739911326ed91
ghannoum74 <abdelrahmangh74@gmail.com>
2024-11-27 04:19:39 +0200

    add imageGetIt reducer

--- client/src/states/solveCustomBoard.ts
+++ client/src/states/solveCustomBoard.ts
@@ -2,10 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 
 type selvedState = {
   isSolved: boolean;
+  isReadIt: boolean;
+  isPending: boolean;
 };
 
 const initialState: selvedState = {
   isSolved: false,
+  isReadIt: false,
+  isPending: false,
 };
 
 export const solveCustomSudoku = createSlice({
@@ -15,9 +19,16 @@ export const solveCustomSudoku = createSlice({
     solveCustomBoard: (state, action) => {
       state.isSolved = action.payload;
     },
+    imageDataGetIt: (state, action) => {
+      state.isReadIt = action.payload;
+    },
+    setIsPending: (state, action) => {
+      state.isPending = action.payload;
+    },
   },
 });
 
 // Correct the export here:
-export const { solveCustomBoard } = solveCustomSudoku.actions;
+export const { solveCustomBoard, imageDataGetIt, setIsPending } =
+  solveCustomSudoku.actions;
 export default solveCustomSudoku.reducer;
